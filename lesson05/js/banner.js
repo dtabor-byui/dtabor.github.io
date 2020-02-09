f2ab5386fa5215bddb2c7ef11fc49310eaac9029
dtabor-byui <tab18001@byui.edu>
2020-02-08 17:11:41 -0700

    yes

--- lesson05/js/banner.js
+++ lesson05/js/banner.js
@@ -1,5 +1,6 @@
 var d = new Date();
 thisDay = d.getDay()
-if (thisDay.getDay() == 5 ) {
-    document.querySelector("aside").style.display = "block";
+if (thisDay.getDay() == 6 ) {
+    document.getElementById("thisDay").innerHTML = "Saturday = Preston Pancakes in the Park! <br> 9:00 a.m. Saturday at the city park pavilion.";
+
   } 
