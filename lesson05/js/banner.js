7d107be23c5adf331830fab1b08c8e789df1a39e
dtabor-byui <tab18001@byui.edu>
2020-02-08 17:07:04 -0700

    yes

---
+++ lesson05/js/banner.js
@@ -0,0 +1,5 @@
+var d = new Date();
+thisDay = d.getDay()
+if (thisDay.getDay() == 5 ) {
+    document.querySelector("aside").style.display = "block";
+  } 
--- lesson05/js/newdate.js
+++ lesson05/js/newdate.js
@@ -3,7 +3,3 @@ thisDay = d.getDay();
 friday = "It is Friday don't forget the pancake breakfast!!";
 var date = (d.getMonth() + 1) + '/' + d.getDate() + '/' + d.getFullYear();
 document.getElementById("date").innerHTML = date;
-
-    if (thisDay == 6) {
-        document.getElementById("thisDay").innerHTML = friday;
-    }
