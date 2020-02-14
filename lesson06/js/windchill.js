6f1fd51849ecd055492d4feced1ebc453e34f9cc
dtabor-byui <tab18001@byui.edu>
2020-02-14 09:24:38 -0700

    commit

--- lesson06/js/windchill.js
+++ lesson06/js/windchill.js
@@ -1,18 +1,16 @@
-function calculate () {
-    var temp = parseFloat (document.getElementById('temp').value);
-    var wind = parseFloat (document.getElementById('wind').value);
-    var itsCold = windChill(temp,wind);
-    if (temp >= 50 && wind >= 4.8) {
-        document.write
-    } 
-    var div = document.getElementById ('').textContent =
-        div.textContent = itsCold;
-    }
+var t = parseFloat(document.getElementById("temp").textContent);
+var s = parseFloat(document.getElementById("wind").textContent);
+if (t <= 50 && s >= 3) {
+    var windchills = windChill(t, s);
+    document.getElementById('windchill').textContent = windchills;
+} else {
+    var windchills = "N/A";
+    document.getElementById('windchill').textContent = windchills;
+}
 
 
-function windChill (temp,wind) {
-    var a = Math.pow(v,.16);
-    var windChill = 35.74 + 0.6215 * t -35.75* a + .4275 * t * a;
-    return windChill;
-    
-    }
+function windChill(t, s) {
+    var f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, .16) + 0.4275 * t * Math.pow(s, .16);
+    var l = Math.round(f * 100) / 100
+    return l;
+}
