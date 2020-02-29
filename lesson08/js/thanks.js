c208704099fb819ede768f1416f9b2671860f0bb
dtabor-byui <tab18001@byui.edu>
2020-02-29 16:48:22 -0700

    y

---
+++ lesson08/js/thanks.js
@@ -0,0 +1,11 @@
+
+        function showMessage(){
+            var name = document.getElementById("fullname").value;
+            var phone = document.getElementById("phone").value;
+            var email = document.getElementById("email").value;
+            var zip = document.getElementById("zip").value;
+            
+            doccument.getElementById("")
+
+            display_message.innerHTML= message;
+        }
