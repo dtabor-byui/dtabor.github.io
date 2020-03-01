907e86c11be29a6d66a416eff6e7c010663171cf
dtabor-byui <tab18001@byui.edu>
2020-02-29 21:18:22 -0700

    y

--- lesson08/js/thanks.js
+++ lesson08/js/thanks.js
@@ -1,11 +1,26 @@
 
-        function showMessage(){
-            var name = document.getElementById("fullname").value;
-            var phone = document.getElementById("phone").value;
-            var email = document.getElementById("email").value;
-            var zip = document.getElementById("zip").value;
-            
-            doccument.getElementById("")
+ var url_string = window.location.href;
+ var url = new URL(url_string);
+ var name = url.searchParams.get("fullname");
+ var phone = url.searchParams.get("phone");
+ var email = url.searchParams.get("email");
+ var date = url.searchParams.get("date");
+ var sev = url.searchParams.get("stormseverity");
+ var stype  = url.searchParams.get("stormtype");
+ var sReg  = url.searchParams.get("stormregion");
+ var danger  = url.searchParams.get("user in-danger level");
+ var comment  = url.searchParams.get("comment");
+ var lb = document.createElement('br');
+ var data = name
+            + '\n' +phone
+            + '\n' +email
+            + '\n' +date
+            + '\n' +sev
+            + '\n' +stype
+            + '\n' +sReg
+            + '\n' +danger
+            + '\n' +comment;
 
-            display_message.innerHTML= message;
-        }
+ document.getElementById('input').innerText = (data);
+        
+        
