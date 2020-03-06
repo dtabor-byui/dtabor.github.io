0db949a02df8d4140707ba5d8d22d19bbed39273
dtabor-byui <tab18001@byui.edu>
2020-03-06 15:23:52 -0700

    y

--- lesson09/js/homepage.js
+++ lesson09/js/homepage.js
@@ -12,22 +12,33 @@ fetch(data)
         for (let i = 0; i < town.length; i++) {
             if (town[i].name == 'Preston' || town[i].name == 'Soda Springs' || town[i].name == 'Fish Haven') {
 
-                let index = document.createElement('article');
+                let index = document.createElement('section');
                 let h2 = document.createElement('h2');
                 let p = document.createElement('p');
                 let image = document.createElement('img');
                 let div = document.createElement('div');
-                div.classList.add('text'):
+                let pop = document.createElement('p');
+                let year = document.createElement('p');
+                let rain = document.createElement('p');
+                div.classList.add('text');
+
 
                 var photo = 'images/' + town[i].photo
+
                 h2.textContent = town[i].name;
-                p.textContent = town[i].motto;
+                p.textContent = 'Motto: ' + town[i].motto;
+                pop.textContent = 'Population: ' + town[i].currentPopulation;
+                year.textContent = 'Year Founded: ' + town[i].yearFounded;
+                rain.textContent = 'Average Rainfall: ' +town[i].averageRainfall;
                 image.setAttribute('src', photo);
 
                 index.appendChild(h2);
                 index.appendChild(image);
                 div.appendChild(p);
-                
+                div.appendChild(pop);
+                div.appendChild(year);
+                div.appendChild(rain);
+
                 index.appendChild(div);
                 
                 
