c88340d81e9214500da6fba063db3e6e4fe2fd73
dtabor-byui <tab18001@byui.edu>
2020-03-14 09:41:27 -0600

    y

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,16 +1,29 @@
-const apiURL="http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=d84c1685f942afd6215201b789f554fa";
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=d84c1685f942afd6215201b789f554fa";
 
 fetch(apiURL)
   .then((response) => response.json())
-  .then((jsObject) => {
-    console.log(jsObject);
 
-    document.getElementById('current-temp').textContent = jsObject.main.temp;
-    document.getElementById('humidity').textContent = jsObject.main.humidity;
-    
-    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-    const desc = jsObject.weather[0].description;  // note how we reference the weather array
-    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-    document.getElementById('icon').setAttribute('alt', desc);
+  .then((jsObject) => {
+    console.log(jsObject); //remove this LINE for production!
+    document.getElementById('currently').textContent = jsObject.weather[0].main;
+    document.getElementById('high').textContent = jsObject.main.temp_max + " ˚F";
+    document.getElementById('humidity').textContent = jsObject.main.humidity + " %";
+    document.getElementById('wind').textContent = jsObject.wind.speed + " mph";
+
+    var t = jsObject.main.temp;
+    var s = jsObject.wind.speed;
+
+    if (t <= 50 && s >= 3) {
+      var windchills = windChill(t, s);
+      document.getElementById('windchill').textContent = windchills;
+    } else {
+      var windchills = " N/A ";
+      document.getElementById('windchill').textContent = windchills;
+    }
+
+    function windChill(t, s) {
+      var f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, .16) + 0.4275 * t * Math.pow(s, .16);
+      var l = Math.round(f * 100) / 100;
+      return l;
+    }
   });
