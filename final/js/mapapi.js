2c190f04785684b47580e81ea24eef3290df375c
dtabor-byui <tab18001@byui.edu>
2020-04-02 17:10:35 -0600

    y

--- final/js/mapapi.js
+++ final/js/mapapi.js
@@ -1,30 +1,35 @@
-
 const data = "https://maps.googleapis.com/maps/api/geocode/json?address=1000+South+Main+Street,%20+Riggins,%20+ID&key=AIzaSyCAmlVQvZ7Gaq58E3-a_CyxOO4--jdy5Ns";
 fetch(data)
-  .then((response) => response.json())
+    .then((response) => response.json())
 
-  .then((jsPosition) => {
-      var results = jsPosition.results[0];
-      var geometry= results.geometry;
-      var location = geometry.location;
-      var myCenter = location;
+    .then((jsPosition) => {
+        var results = jsPosition.results[0];
+        var geometry = results.geometry;
+        var location = geometry.location;
+        var faddress = results.formatted_address;
+        var myCenter = location;
+        var main = document.createElement('div');
+        var p = document.createElement('p');
 
-      function initialize(){
-    var mapProp = {
-        center:myCenter,
-        zoom:15,
-        mapTypeId:google.maps.MapTypeId.ROADMAP
-    };
+        p.textContent = faddress;
+        main.appendChild(p);
 
-    var map = new google.maps.Map(document.getElementById("map"),mapProp);
+        document.querySelector('div.address').appendChild(main);
 
-    var marker = new google.maps.Marker({
-        position:myCenter,
-    });
+        function initialize() {
+            var mapProp = {
+                center: myCenter,
+                zoom: 15,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
 
-    marker.setMap(map);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
-  })
+            var map = new google.maps.Map(document.getElementById("map"), mapProp);
 
+            var marker = new google.maps.Marker({
+                position: myCenter,
+            });
 
+            marker.setMap(map);
+        }
+        google.maps.event.addDomListener(window, 'load', initialize);
+    })
